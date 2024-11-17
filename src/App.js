a2196937afa743cd8be89bf94e7d83fb94c71095
Serhan Yılmaz <56499118+serhanylmz@users.noreply.github.com>
2024-11-18 01:39:27 +0300

    fix rerouting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { ShoppingCart, User } from 'lucide-react';
-import './App.css';
-import RegisterPage from './Pages/RegisterPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import ProductsPage from './Pages/ProductsPage';
+import ProductDetailsPage from './Pages/ProductDetailsPage';
+import CartPage from './Pages/CartPage';
+import PaymentPage from './Pages/PaymentPage';
+import ThankYouPage from './Pages/ThankYouPage';
+import { LoginPage, SignupPage } from './Pages/AuthPages';
 
-
-export default function App() {
+function App() {
   return (
-    <div>
-      <RegisterPage/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/product/:id" element={<ProductDetailsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/thank-you" element={<ThankYouPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
