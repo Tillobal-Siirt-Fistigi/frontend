d4e30005b2152472520fd88eaefaab657aed06b1
Sadiq04 <81559141+Sadiq04@users.noreply.github.com>
2024-11-18 19:13:33 +0300

    Add to Cart backend integrated

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ import CartPage from './Pages/CartPage';
 import PaymentPage from './Pages/PaymentPage';
 import ThankYouPage from './Pages/ThankYouPage';
 import { LoginPage, SignupPage } from './Pages/AuthPages';
+import AuthProvider from './contexts/AuthContext';  // Import the AuthProvider
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/product/:id" element={<ProductDetailsPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/payment" element={<PaymentPage />} />
-        <Route path="/thank-you" element={<ThankYouPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/product/:id" element={<ProductDetailsPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/thank-you" element={<ThankYouPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
